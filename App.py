e68a97c5c46018d67b331e8ca5bdcf9d12356856
john <jmwangi288@gmail.com>
2020-10-06 20:08:56 +0300

    postgres db

--- App.py
+++ App.py
@@ -1,15 +1,37 @@
 
 from flask import Flask, render_template
-from flask_mail import Mail, Message
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.secret_key = 'Secret Key'
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mwangi:.D3v3l0p3r!@localhost/Employee'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    department = db.Column(db.String(100))
+
+    def __init__(self, name, email, department):
+        self.name = name
+        self.email = email
+        self.department = department
 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/', methods = ['GET'])
+
+@app.route('/', methods=['GET'])
 def Home():
     return render_template("index.html")
 
+
 if __name__ == "__main__":
     app.run()
+    
